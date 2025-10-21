e44405bd709feca49d799afd010794db3b00e3f4
Richard Rigby <r.rigby@leeds.ac.uk>
2025-10-21 11:06:09 +0100

    Remove release version from config
    Is this:
    
      release = '1'
    
    causing the '1' in page titles?:
    
      <title> ... cemac.github.io 1 documentation</title>

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'cemac.github.io'
 copyright = '2025, Centre for Environmental Modelling and Computation'
 author = 'Centre for Environmental Modelling and Computation'
-release = '1'
+release = ''
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
